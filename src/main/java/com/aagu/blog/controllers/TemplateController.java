4cb88102af15600fc2dac756e92c914cc9a29062
aagu <aaguisme@gmail.com>
2019-05-13 18:50:10 +0800

    day 2 -- 前端页面

--- src/main/java/com/aagu/blog/controllers/TemplateController.java
+++ src/main/java/com/aagu/blog/controllers/TemplateController.java
@@ -4,14 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class TemplateController {
 
-    @RequestMapping(value = "blog")
-    String main(HttpServletRequest request) {
-        request.setAttribute("key", "hello world");
+    @RequestMapping(value = "")
+    String main() {
         return "index";
+    }
+
+    @RequestMapping(value = "index")
+    void index(HttpServletResponse response) {
+        try {
+            response.sendRedirect("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping(value = "admin")
@@ -19,5 +29,20 @@ public class TemplateController {
         request.setAttribute("greeting", "Hi");
         request.setAttribute("key", "admin");
         return "admin";
+    }
+
+    @RequestMapping(value = "blog")
+    String blog() {
+        return "blog";
+    }
+
+    @RequestMapping(value = "about")
+    String about() {
+        return "about";
+    }
+
+    @RequestMapping(value = "contact")
+    String contact() {
+        return "contact";
     }
 }
