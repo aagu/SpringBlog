d19ba1337a566ab189d0606c5010bcc9f957a44b
aagu <aaguisme@gmail.com>
2020-06-06 16:34:56 +0800

    https缓存备案图标

--- src/main/resources/workspace/src/components/Footer.vue
+++ src/main/resources/workspace/src/components/Footer.vue
@@ -25,7 +25,7 @@
         <span class="caption" style="color:#939393;">浙ICP备20010527号-1</span>
         <div style="margin:0 auto;">
           <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33011802001968" style="display:flex;text-decoration:none;height:20px;line-height:20px;">
-            <img src="http://www.beian.gov.cn/img/ghs.png" style="width: 16px; height: 16px"/>
+            <img src="https://img.aagu.ltd/ghs.png" style="width: 16px; height: 16px"/>
             <p class="caption" style="float:left;margin: 0 0 0 5px; color:#939393;">浙公网安备 33011802001968号</p>
           </a>
         </div>
